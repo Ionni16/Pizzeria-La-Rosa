a9dbe51727802c7c047bfda81ba07cf1d0a85662
Ionni16 <ionut1.puiu@gmail.com>
2026-04-03 17:30:40 +0200

    sistemato bug foto

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,11 @@ const nextConfig: NextConfig = {
         hostname: "vylfhmsutvwdvgussrkn.supabase.co",
         pathname: "/storage/v1/object/public/**",
       },
+      {
+        protocol: "http",
+        hostname: "vylfhmsutvwdvgussrkn.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
     ],
   },
 };
